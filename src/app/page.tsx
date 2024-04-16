71650c387c8ff122a4e1e1e41d18610d49e39dab
ejimkaraonyec <ejimkaraonyec@gmail.com>
2024-04-16 10:06:26 +0100

    use images from database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,28 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/40278dd5-236f-4a09-8d2a-89efc8857458-8x3hkn.jpg",
-  "https://utfs.io/f/121687ed-ef86-4540-b362-b80c632cd949-17x8q8.png",
-];
+// const mockUrls = [
+//   "https://utfs.io/f/40278dd5-236f-4a09-8d2a-89efc8857458-8x3hkn.jpg",
+//   "https://utfs.io/f/121687ed-ef86-4540-b362-b80c632cd949-17x8q8.png",
+// ];
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+// const mockImages = mockUrls.map((url, index) => ({
+//   id: index + 1,
+//   url,
+// }));
 
 export default async function HomePage() {
   const images = await db.query.images.findMany();
 
-  console.log({ images });
+  // console.log({ images });
 
   return (
-    <main className="container mx-auto min-h-screen">
+    <main className="container mx-auto min-h-screen py-8">
       <div className="flex flex-wrap gap-3">
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {[...images, ...images, ...images].map((image, index) => (
           <div
             key={image.id + "-" + index}
-            className="w-[150px] overflow-hidden rounded-md"
+            className="w-[200px] flex-grow overflow-hidden rounded-md"
           >
             <Image
               src={image.url}
